2519a2e3b3d35b4d05b209aafb363952c4acb7b0
sergey-komar <kursev81@yandex.ru>
2024-01-24 22:46:47 +0300

    start

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,43 @@
 $(function () {
 
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    const tabsItem = document.querySelector('.tabs__item');
+const tabsItemBtn = document.querySelectorAll('.tabs__item-btn');
+const tabsContent = document.querySelectorAll('.tabs__content');
+
+function tabsHide(){
+    tabsContent.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show');
+    });
+
+    tabsItemBtn.forEach(btn =>{
+        btn.classList.remove('tabs-active');
+    })
+}
+function tabsShow(i){
+    tabsContent[i].classList.add('show');
+    tabsContent[i].classList.remove('hide');
+    tabsItemBtn[i].classList.add('tabs-active');
+}
+
+tabsItem.addEventListener('click', (e)=>{
+    const target = e.target;
+    
+    if(target && target.classList.contains('tabs__item-btn')){
+        tabsItemBtn.forEach((item, i)=>{
+            if(target == item){
+                tabsHide();
+                tabsShow(i);
+            }
+           
+        })
+       
+    }
+})
+
+tabsHide();
+tabsShow(0);
+})
