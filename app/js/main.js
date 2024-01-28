4ed3ffa40cd6d721aa253d4a0769a2bca6504078
sergey-komar <kursev81@yandex.ru>
2024-01-28 19:23:56 +0300

    4 page mobile

--- app/js/main.js
+++ app/js/main.js
@@ -245,6 +245,16 @@ if(tabsItemCalculator && tabsItemBtnCalculator && tabsContentCalculator){
             });
         }
         
+        //menu-mobile
+        const menu = document.querySelector('.menu');
+        const mobile = document.querySelector('.nav-icon');
+        
+        mobile.addEventListener('click', function(){
+            this.classList.toggle('nav-icon--active');
+            menu.classList.toggle('nav--active');
+        
+        });
 
+       
 
 })
